7ccdfc11bc72e62bd32a4b9e43551f24968d4a32
brice <bricemarcais62220@gmail.com>
2024-09-12 16:21:39 +0200

    ✨ connection bdd

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const app = express();
 const router = require("./src/routes/router");
+const mysql = require("mysql2");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "0000",
+  database: "db",
+});
+connection.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion bdd:" + err);
+  } else {
+    console.log("Connecter à la bdd");
+  }
+});
 
 app.use(express.static("public"));
 
